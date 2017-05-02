5ecc5f77eb8004534ec03182287e43ac88124462
mclavin <lavin.martin@gmail.com>
2017-05-02 11:27:05 +0200

    estbalished database connection

---
+++ database/connection.php
@@ -0,0 +1,13 @@
+<?php
+    $options = [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        PDO::ATTR_EMULATE_PREPARES   => false
+    ];
+
+    $pdo = new pdo(
+        'mysql:host=localhost:8889;dbname=mym;charset=utf8',
+        'root',
+        'root',
+        $options
+    );
---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    include("database/connection.php");
+?>
+<html>
+    <head>
+        <title>MYM</title>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>MYM</h1>
+    </body>
+</html>
