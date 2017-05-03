e2714d7ed292f915740f7d7c7a8d70f331e17f46
magnusohrstrom <magnusohrstrom330@gmail.com>
2017-05-03 14:21:32 +0200

    db change

--- partials/navbar.php
+++ partials/navbar.php
@@ -13,7 +13,7 @@
         <a class="nav-link" href="#">Add post</a>
       </li>
       <li class="nav-item">
-        <a class="nav-link disabled" href="#">Sign up</a>
+        <a class="nav-link" href="#">Sign up</a>
       </li>
     </ul>
 
