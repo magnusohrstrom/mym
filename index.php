3a839a817a3e7d895c7efb095eecc5caa44f6bad
magnusohrstrom <magnusohrstrom330@gmail.com>
2017-05-04 12:37:33 +0200

    adding sass thursday

--- index.php
+++ index.php
@@ -6,12 +6,17 @@
 <!DOCTYPE html>
 <html>
     <head>
+
       <?php include("partials/head.php");?>
 
     </head>
     <body>
       <!--header-->
+      <header>
+        <a id="nav-title" href="../index.php">MYM</a>
         <?php include 'partials/navbar.php';?>
+      </header>
+
       <!--mainsection-->
       <main class= "container">
         <section class="hero">
@@ -19,8 +24,8 @@
           <h2>Social posting!</h2>
           <?php include 'partials/login-form.php'; ?>
         </section>
-        <section class="row post-section">
-          <article class="col-sm-6">
+        <section class=" col-sm-6 post-section">
+          <article class="">
             <h3>title</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></article>
         </section>
@@ -32,5 +37,10 @@
         <?php include 'partials/post-form.php'; ?>
 
       </main>
+      <?php include 'partials/footer.php'; ?>
+      <script src="https://code.jquery.com/jquery-3.1.1.min.js"integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="crossorigin="anonymous"></script>
+      <script src="scripts/main.js" type="text/javascript">
+
+      </script>
     </body>
 </html>
