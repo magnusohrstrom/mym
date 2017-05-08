ba72fde22ef097456dcf443807e58d3d5b329bb1
mclavin <lavin.martin@gmail.com>
2017-05-08 10:29:35 +0200

    hej

--- database/connection.php
+++ database/connection.php
@@ -2,17 +2,17 @@
 class db
 {
 
-    private $options = [
+    private static $options = [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES   => false
     ];
 
-    public static function connection($options)
+    public static function connection()
     {
         return new PDO('mysql:host=localhost:8889;dbname=mym;charset=utf8',
             'root',
             'root',
-            $options);
+            self::$options);
     }
 }
